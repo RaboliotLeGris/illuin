d592852e4a0d4b010ffcdd4b98fccaf81813f234
Raboliot le gris <jordan@raboland.fr>
2021-05-18 17:34:48 +0200

    Delete Dockerfile

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PORT="${PORT:-8080}"
-STORAGE_FOLDER="${ILLUIN_STORAGE_FOLDER:-"/illuin/data"}"
-BASE_URL="${ILLUIN_BASE_URL:-"/"}"
-
-echo "ENV VAR ARE: $PORT $STORAGE_FOLDER"
-
-exec "$@" "--port" "$PORT" "--storage-path" "$STORAGE_FOLDER" "--base-route" "$BASE_URL"
